650cfaa3c0926df0afd665546db21b63e29f78d8
YaraYara <morozova.yara@gmail.com>
2015-11-03 18:47:44 +0200

    Created styled popup for 'Contact Us' form (resolve #1). Fixed responsive design for employees blocks (resolve #3).

--- client/views/home/_formContacts.js
+++ client/views/home/_formContacts.js
@@ -1,10 +1,20 @@
 Template._formContacts.events({
   'click .button.-wide': function(){
-      var name = $('#name').val(),
-          email = $('#email').val(),
-          message = $("#message").val();
-      var text = "Message from: " + name + "\rEmail: " + email + "\rContent:" + message;
+    var name = $('#name').val(),
+        email = $('#email').val(),
+        message = $("#message").val();
+    var text = "Message from: " + name + "\rEmail: " + email + "\rContent:" + message;
 
-      Meteor.call('sendEmail', email, text);
+    Meteor.call( 'sendEmail', email, text, function(error, result) {
+      if(error) {
+        Modal.show( '_sendEmailPopup', {'message': 'An error occurred.'} );
+      } else {
+        Modal.show( '_sendEmailPopup', {'message': 'Your message was sent successfully.'} );
+        $('#name').val( '' );
+        $('#email').val( '' );
+        $('#message').val( '' );
+      }
+    });
+    return false;
   }
 });
