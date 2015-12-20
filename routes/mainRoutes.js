6416ec79ee8b7aee6d3e6449cbb7ccca741c7fe7
Epaminond <k4merton@gmail.com>
2015-12-20 23:36:24 +0200

    SEO optimization: title and description

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,7 +1,12 @@
 Router.route('/', {
   name: 'home',
   onAfterAction: function () {
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
+    SEO.set({
+      title: 'Keenethics: Advanced Web Development Services',
+      meta: {
+        description: 'Let us improve your business using modern JavaScript frameworks like Meteor.js, React or MEAN Stack.'
+      },
+    });
   },
   waitOn: function() {
     return [
