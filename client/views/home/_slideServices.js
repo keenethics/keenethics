e9e22cfaa3c0ab87bb57b370f0850c866c792450
yataras <taras.yaskiv@gmail.com>
2015-12-23 02:26:58 +0200

    add structured data for technologies

---
+++ client/views/home/_slideServices.js
@@ -0,0 +1,24 @@
+Template._slideServices.helpers({
+  technologies: () => [
+    {
+      name:        'node.js',
+      development: 'back end',
+    },
+    {
+      name:        'meteor.js',
+      development: 'full stack',
+    },
+    {
+      name:        'Angular',
+      development: 'front end',
+     },
+     {
+       name:        'React',
+       development: 'front end',
+     },
+     {
+       name:        'MEAN',
+       development: 'full stack',
+     }
+  ]
+});
