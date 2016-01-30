9b34c849ea757583276587a48c6faa7f8ea0d2c3
Epaminond <k4merton@gmail.com>
2016-01-30 15:20:38 +0200

    Refactor contacts form

--- client/views/home/_formContacts.js
+++ client/views/home/_formContacts.js
@@ -1,22 +1,23 @@
 Template._formContacts.events({
-  'click .button.-wide': function(){
-    var name = $('#name').val(),
-        email = $('#email').val(),
-        message = $("#message").val();
-    var text = "Message from: " + name + "\rEmail: " + email + "\rContent:" + message;
+  'submit .js-contact-form': function(e, t){
+    e.preventDefault();
+    var name = t.$('#name').val(),
+        email = t.$('#email').val(),
+        message = t.$("#message").val();
     var emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     if (!email || !emailRegEx.test(email) || !message) {
       return Modal.show( '_sendEmailPopup', {'message': 'Email and message required.'} );
     }
+
+    var text = "Message from: " + name + "\rEmail: " + email + "\rContent:" + message;
     Meteor.call( 'sendContactForm', text, function(error, result) {
       if(error) {
         Modal.show( '_sendEmailPopup', {'message': 'An error occurred.'} );
       } else {
         Modal.show( '_sendEmailPopup', {'message': 'Your message was sent successfully.'} );
-        $('.js-contact-form-input').val( '' );
+        t.$('.js-contact-form').trigger('reset');
       }
     });
-    return false;
   }
 });
