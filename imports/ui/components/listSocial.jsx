d9fcd358eacb1526449d367e2bfceee0e440c686
Epaminond <k4merton@gmail.com>
2016-06-26 21:31:28 +0300

    Make social links rotate

--- imports/ui/components/listSocial.jsx
+++ imports/ui/components/listSocial.jsx
@@ -5,34 +5,34 @@ export default class ListSocial extends React.Component {
     return (
       <ul className="list-social row">
         <li className="list-social--item">
-            <a href="https://www.upwork.com/o/companies/~0106b5437592391f94/" className="list-social--link">
-              <img src="/images/social/upwork.svg" />
-            </a>
+          <a href="https://www.upwork.com/o/companies/~0106b5437592391f94/">
+            <img src="/images/social/upwork.svg" className="list-social--link" />
+          </a>
         </li>
         <li className="list-social--item">
-            <a href="https://www.facebook.com/KeenEthics.development" className="list-social--link">
-              <img src="/images/social/fb.svg" />
-            </a>
+          <a href="https://www.facebook.com/KeenEthics.development">
+            <img src="/images/social/fb.svg" className="list-social--link" />
+          </a>
         </li>
         <li className="list-social--item">
-            <a href="https://plus.google.com/103030720036135400511/about" className="list-social--link">
-              <img src="/images/social/g+.svg" />
-            </a>
+          <a href="https://plus.google.com/103030720036135400511/about">
+            <img src="/images/social/g+.svg" className="list-social--link" />
+          </a>
         </li>
         <li className="list-social--item">
-            <a href="https://twitter.com/keen_ethics" className="list-social--link">
-              <img src="/images/social/twitter.svg" />
-            </a>
+          <a href="https://twitter.com/keen_ethics">
+            <img src="/images/social/twitter.svg" className="list-social--link" />
+          </a>
         </li>
         <li className="list-social--item">
-            <a href="https://github.com/keenethics" className="list-social--link">
-              <img src="/images/social/git.svg" />
-            </a>
+          <a href="https://github.com/keenethics">
+            <img src="/images/social/git.svg" className="list-social--link" />
+          </a>
         </li>
         <li className="list-social--item">
-            <a href="https://blog.keenethics.com/" className="list-social--link">
-              <img src="/images/social/medium.svg" />
-            </a>
+          <a href="https://blog.keenethics.com/">
+            <img src="/images/social/medium.svg" className="list-social--link" />
+          </a>
         </li>
       </ul>
     );
