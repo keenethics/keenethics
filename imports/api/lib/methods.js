97e68939c1a63dd002f2c316ef564290b913d245
Mykhaylo Fedorovych <misha.fedorovych@gmail.com>
2016-07-05 11:07:35 +0300

    Fix exceptions

--- imports/api/lib/methods.js
+++ imports/api/lib/methods.js
@@ -1,10 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import ParsingLib from './parsingLib';
-import GetNewsData from './getNewsData'
+import { fetchNews } from './news';
 
 import _ from 'underscore';
 import { check } from 'meteor/check';
 import { Email } from 'meteor/email';
+import moment from 'moment';
 
 Meteor.methods({
   sendContactForm(text) {
@@ -24,17 +24,10 @@ Meteor.methods({
 
   getPosts() {
     this.unblock();
-    const parsingLib = new ParsingLib();
-    const getNewsData = new GetNewsData();
-    const mediumResult = parsingLib.parseMedium(getNewsData.getMediumJson());
-    const meetupResult = parsingLib.parseMeetup(getNewsData.getMeetupJson());
-
-    const result = _.union(_.values(mediumResult), meetupResult);
-    const sorted = _.sortBy(result, 'publishedDate').reverse().slice(0, 4);
+    const sorted = _.sortBy(fetchNews(), 'publishedDate').reverse().slice(0, 4);
 
     _.each(sorted, (value, key) => {
-      const splDate = new Date(value.publishedDate).toString().split(' ');
-      sorted[key].publishedDate = `${splDate[1]} ${splDate[2]} ${splDate[3]} `;
+      sorted[key].publishedDate = moment(value.publishedDate).format('MMMM DD YYYY');
     });
 
     return sorted;
