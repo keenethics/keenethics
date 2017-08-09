f0de943bfd4447afd45a5bd294ad34a352389661
Sergey Gornostaev <sergey@gornostaev.pro>
2017-08-09 09:42:20 +0300

    Provide sentry.io

--- components/layout/head.js
+++ components/layout/head.js
@@ -1,3 +1,5 @@
+/* global Raven */
+
 import Head from 'next/head';
 import Router from 'next/router';
 
@@ -8,16 +10,28 @@ Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
-const LayoutHead = () => (
-  <Head>
-    <title>KEEN.ETHICS</title>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link type="text/css" rel="stylesheet" href="/static/main.css" />
-    <link type="image/vnd.microsoft.icon" rel="icon" href="/static/images/favicon.ico" />
-    <link type="image/x-icon" rel="shortcut icon" href="/static/images/favicon.ico" />
-    <script type="text/javascript" src="https://secure.skypeassets.com/i/scom/js/skype-uri.js" />
-  </Head>
-);
+export default class LayoutHead extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default LayoutHead;
+    this.state = {
+    };
+  }
+  componentDidMount() {
+    Raven.config('https://354718bb50184a3f8b2d6561c650daec@sentry.io/201137').install();
+  }
+  render() {
+    return (
+      <Head>
+        <title>KEEN.ETHICS</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link type="text/css" rel="stylesheet" href="/static/main.css" />
+        <link type="image/vnd.microsoft.icon" rel="icon" href="/static/images/favicon.ico" />
+        <link type="image/x-icon" rel="shortcut icon" href="/static/images/favicon.ico" />
+        <script type="text/javascript" src="https://secure.skypeassets.com/i/scom/js/skype-uri.js" />
+        <script src="https://cdn.ravenjs.com/3.17.0/raven.min.js" crossOrigin="anonymous" />
+      </Head>
+    );
+  }
+}
