bbcdbcb599465d3f8cd7ba51b8c996fde6a2efaa
Sergey Gornostaev <sergey@gornostaev.pro>
2017-08-24 10:16:20 +0300

    Projects styles

--- pages/project-black-book.js
+++ pages/project-black-book.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Layout from '../components/layout/main';
 
-const baseColor = '#ee521d';
+const baseColor = '#3c67ab';
 
 const ProjectComicEnglish = ({ url }) => (
   <Layout currentURL={url}>
@@ -13,38 +13,61 @@ const ProjectComicEnglish = ({ url }) => (
           <div className="project-content-summary" style={{ background: baseColor }}>
             <div className="project-content-summary-inner">
               <h2 className="title">Black Book</h2>
-              <h3 className="duration"><span>Duration:</span> 5 months</h3>
+              <h3 className="duration"><span>Duration:</span> 6 months, one month left. 1 developer.</h3>
               <p className="description">
-                Service for learning foreign languages in a gaming form. It was designed as a
-                chat-bot that suggested user to pass different quests that required improving
-                English.
+                Display the statistics of company employees
               </p>
               <div className="tags">
                 <div className="tag-title">Technologies:</div>
                 <div className="tag" style={{ color: baseColor }}>Meteor</div>
                 <div className="tag" style={{ color: baseColor }}>Ionic</div>
+                <div className="tag" style={{ color: baseColor }}>Angular 2</div>
+                <div className="tag" style={{ color: baseColor }}>D3</div>
               </div>
             </div>
             <div className="project-content-preview">
               <div className="project-content-preview-inner">
-                <img src="/static/images/project/macbook.png" alt="macbook" />
+                <img src="/static/images/project/macbook.png" alt="macbook" className="macbook" />
+                <img src="/static/images/project/black-book.png" alt="Nokia Black Book" className="screen" />
               </div>
             </div>
           </div>
           <div className="project-content-more">
             <p>
-              Service for learning foreign languages in a gaming form. It was designed as a chat-bot
-              that suggested user to pass different quests that required improving English.
+              An app helps track statistics on company employees around the world in real time. An
+              enterprise solution for a company with numerous employees.
             </p>
-            <p>
-              An idea was pretty cool: people like games, but they don’t always like to study. So
-              why not create a game that would teach you skills required in your real-life? That’s
-              what ComicEnglish was about.
-            </p>
-            <p>
-              Keenethics participated in developing MVP and was able to complete required tasks on
-              time. The website is currently offline.
-            </p>
+            <div className="project-achievements">
+              <h3>Achievements</h3>
+              <ul>
+                <li>The project is being built from scratch by Keenethics;</li>
+                <li>
+                  Accepted MVP and preceding development process of the next versions. We enabled
+                  customer’s analytics and top management to keep an eye on the reports, to filter
+                  data and to have an access off-line;
+                </li>
+                <li>
+                  Keenethics developer worked in according to team’s agile methodology, which means
+                  he had to adjust constantly.
+                </li>
+              </ul>
+            </div>
+            <div className="project-preview">
+              <p>
+                Testing server you can have a look at:
+                <span>Login: user@mail.com</span>
+                <span>Password: 111111</span>
+              </p>
+              <a
+                href="https://knotel.com"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="link"
+                style={{ background: baseColor }}
+              >
+                Look at the project
+              </a>
+            </div>
           </div>
         </div>
       </div>
