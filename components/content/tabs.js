555d8b04b47c2f5d66c0e65ab4d046ac93062f9f
Sergey Gornostaev <sergey@gornostaev.pro>
2017-11-20 10:03:48 +0200

    Add tabs init

--- components/content/tabs.js
+++ components/content/tabs.js
@@ -39,76 +39,94 @@ const listOfTabs = points => (
     </div>
   ))
 );
-const PageTabs = ({ points }) => {
-  const sliderSetting = {
-    dots: points.length > 6,
-    arrows: points.length > 6,
-    infinite: true,
-    adaptiveHeight: false,
-    speed: 100,
-    slidesToShow: points.length > 6 ? 6 : points.length,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    draggable: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: points.length > 4 ? 4 : points.length,
-          slidesToScroll: 1,
-          dots: points.length > 4,
-          arrows: points.length > 4,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: points.length > 3 ? 3 : points.length,
-          slidesToScroll: 1,
-          dots: points.length > 3,
-          arrows: points.length > 3,
-        },
-      },
-      {
-        breakpoint: 440,
-        settings: {
-          slidesToShow: points.length > 3 ? 3 : points.length,
-          slidesToScroll: 1,
-          dots: true,
-          arrows: true,
-          draggable: true,
-        },
-      },
-      {
-        breakpoint: 375,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-          arrows: true,
-          draggable: true,
-        },
-      },
-    ],
-  };
 
-  return (
-    <div className="content-tabs">
-      <div className="content-tabs-inner">
-        <Slider {...sliderSetting}>{listOfTabs(points)}</Slider>
+export default class PageTabs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { points } = props;
+
+    this.state = {
+      sliderIsActive: false,
+    };
+    this.sliderInit = this.sliderInit.bind(this);
+    this.sliderSetting = {
+      dots: points.length > 6,
+      arrows: points.length > 6,
+      infinite: true,
+      adaptiveHeight: false,
+      speed: 100,
+      slidesToShow: points.length > 6 ? 6 : points.length,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      init: this.sliderInit,
+      draggable: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: points.length > 4 ? 4 : points.length,
+            slidesToScroll: 1,
+            dots: points.length > 4,
+            arrows: points.length > 4,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: points.length > 3 ? 3 : points.length,
+            slidesToScroll: 1,
+            dots: points.length > 3,
+            arrows: points.length > 3,
+          },
+        },
+        {
+          breakpoint: 440,
+          settings: {
+            slidesToShow: points.length > 3 ? 3 : points.length,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true,
+            draggable: true,
+          },
+        },
+        {
+          breakpoint: 375,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true,
+            draggable: true,
+          },
+        },
+      ],
+    };
+  }
+  sliderInit() {
+    this.setState({
+      sliderIsActive: true,
+    });
+  }
+  render() {
+    const { points } = this.props;
+    const { sliderIsActive } = this.state;
+
+    return (
+      <div className="content-tabs">
+        <div className={sliderIsActive ? 'content-tabs-inner slider-active' : 'content-tabs-inner'}>
+          <Slider {...this.sliderSetting}>{listOfTabs(points)}</Slider>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 PageTabs.propTypes = {
   points: PropTypes.arrayOf(PropTypes.object),
   withDescription: PropTypes.bool,
 };
-
 PageTabs.defaultProps = {
   points: [],
   withDescription: false,
 };
-
-export default PageTabs;
