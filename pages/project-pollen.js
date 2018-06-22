c9671f28d80565c598ba5d825faa570deb111ddb
kurtianyk <o.kurtianyk@gmail.com>
2018-06-22 12:41:48 +0300

    update case studies

--- pages/project-pollen.js
+++ pages/project-pollen.js
@@ -28,27 +28,64 @@ const ProjectPollen = ({ url }) => (
           </div>
           <div className="project-content-more">
             <p>
-              An idea behind this application was to help people organize social-campaigns through
-              email-contacts that campaign-supporters have. This could serve as an alternative to
-              Facebook and other social-networks in some cases.
+              Just a few clicks to boost the sales on Facebook and all over the Internet.
             </p>
             <p>
-              Let’s say you want to support law that restricts production of some products dangerous
-              for the environment. In a few clicks you can create a campaign and add your friends’
-              emails to it with a message that you’ll appreciate if they spread information on this
-              campaign to their friends. Each of your friends can do the same thing spreading it
-              wider to their friends which finally may have everyone involved in the campaign given
-              people really support it.
+              Simply put, Pollen is a service for organizing social-campaigns via Gmail contacts.
+              This is a simple and cost-effective automation platform for marketing that assists
+              the businesses in reaching more customers with the help of the highly targeted ad campaigns.
             </p>
-            <div className="project-achievements">
-              <h3>Achievements</h3>
-              <ul>
-                <li>
-                  Project was heavily refactored and it’s size was reduced by 10 000 strings of
-                  code. At the same time it’s stability was increased and multiple bugs were fixed.
-                </li>
-              </ul>
-            </div>
+            <div className="title">How Pollen Works?</div>
+            <p>
+              Supposing, Jack has developed some eco-friendly substitute to plastic and he wants to get a
+              support from other people who are also concerned about the environment. Just in a few clicks,
+              he creates a campaign and adds the emails of his friends who might be interested in that topic.
+              In the text of the message, he asks the people to spread this message to their friends. In
+              turn, each of his friends who receive the message is encouraged to do the same thing. As the
+              result, the message spreads wider and wider. More and more genuinely interested people learn
+              about the product, Jack increases the awareness about his idea and receives the support at the
+              same time. This approach is extremely effective because it involves potentially interested audience
+              only, and this is exactly what Pollen does automatically.
+            </p>
+            <p>
+              Pollens uses fewer bullets, but it most certainly hits the target.
+            </p>
+            <div className="title">The Idea and Technology behind Pollen</div>
+            <p>
+              An idea that stands behind this application is very simple: it was developed as the means to assist
+              people in the organization of social-campaigns using the email-contacts of campaign-supporters.
+            </p>
+            <p>
+              Pollen develops an email list of a store, comes up with website engagement and social page
+              engagement, and based on the analysis of their current traffic and buyers, it automatically
+              builds similar audiences. This self-serve marketing automation platform assists the online
+              retailers in acquiring new customers on both Facebook and other websites.
+            </p>
+            <p>
+              Pollen allows the user to launch the complex acquisition and conversion driven campaigns.
+              The setup takes around two minutes, whereas the new customers keep coming long after that.
+              The application automatically adjusts the campaigns as they run. Regardless of the budget
+              for the campaign, each is treated with equal dedication and optimized to outperform the competition.
+            </p>
+            <p>
+              The machine learning technology enables Pollen to conduct the analysis of online buying patterns.
+              Thus, the app will show the ads only to those prospects who are most likely to become the customers.
+              This is the simple yet powerful solution that helps the business owners get the most from their business.
+            </p>
+            <div className="title">Pollen: Ensuring the Efficiency</div>
+            <p>
+              The application automatically conducts A/B testing of Ad Creative to allocate the
+              budget in a smart way - by promoting the top performing ads first.
+            </p>
+            <p>
+              Pollen also optimizes the client budget to cover not only prospecting but also retargeting. This
+              particular function enables the acquisition of new prospects and fosters their converting into customers.
+            </p>
+            <p>
+              All in all, this is the app that automates a lot of processes and gives the business owners peace of mind.
+              It is basically an advertising autopilot that triples the effectiveness of business processes by turning
+              the advertising into a completely hands-free experience.
+            </p>
           </div>
           <Navigation url={url} />
         </div>
