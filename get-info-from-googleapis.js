4a72094acf1e49294ebb26c1205f3c1f0dbde7a3
Serhii <serhii.sakhniuk@gmail.com>
2018-06-27 13:33:34 +0300

    Add batchGet (get data from multiple sheets)

--- get-info-from-googleapis.js
+++ get-info-from-googleapis.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 //FIXME: export to env variables
 const TEAM_SHEET_ID = '18PXulnsVlgrHFHh7YLz6bkxypTlir8tZLeZWHNiGiyI';
-const TEAM_RANGE = 'team';
+const ranges = ['team', 'careers'];
 const CREDS_FILE = 'dauntless-theme-207811-a063c4c9e36d.json';
 const scopes = ['https://www.googleapis.com/auth/drive.readonly'];
 const TIME_TO_LIVE = 30 * 60 * 1000;
@@ -35,25 +35,31 @@ async function getTeam() {
     return Promise.resolve(teamCache);
   }
 
-  const client = await google.auth.getClient({
-    keyFile: path.join(__dirname, CREDS_FILE),
-    scopes,
-  });
 
-  const sheets = google.sheets('v4');
+  try {
+    const client = await google.auth.getClient({
+      keyFile: path.join(__dirname, CREDS_FILE),
+      scopes,
+    });
 
-  const table = await sheets.spreadsheets.values.get({
-    auth: client,
-    spreadsheetId: TEAM_SHEET_ID,
-    range: TEAM_RANGE,
-  });
+    const sheets = google.sheets('v4');
 
-  let { values } = table.data;
-  // Filter out empty lines
-  values = values.filter(v => v.length > 3);
-  teamCache = arrayOfArraysToCollection(values);
-  lastUpdateTime = +new Date();
-  return Promise.resolve(teamCache);
+    const table = await sheets.spreadsheets.values.batchGet({
+      auth: client,
+      spreadsheetId: TEAM_SHEET_ID,
+      ranges: ranges
+    });
+
+
+    let { values } = table.data;
+    // Filter out empty lines
+    values = values.filter(v => v.length > 3);
+    teamCache = arrayOfArraysToCollection(values);
+    lastUpdateTime = +new Date();
+    return Promise.resolve(teamCache);
+  } catch (e) {
+    console.error( e );
+  }
 }
 
 
