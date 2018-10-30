270bf82ddb0a4e0f5502be2f11711050fd072532
soloviovmax <solovyov.amron@gmail.com>
2018-10-30 12:14:39 +0200

    Updated 'Prototype' article (#89)

--- postsort.config.js
+++ postsort.config.js
@@ -8,7 +8,6 @@ const postsDatePair = [
   { createdAt: 1515590319000, filename: '1515590319000-5-unexpected-ways-to-use-blockchain-technology.md' },
   { createdAt: 1517306255770, filename: '1517306255770-docker-5-cases-when-you-should-not-use-it.md' },
   { createdAt: 1517325231241, filename: '1517325231241-stop-losing-your-staff.md' },
-  { createdAt: 1517766794846, filename: '1517766794846-what-is-prototype-and-how-to-eat-it-in-a-right-way.md' },
   { createdAt: 1521631041972, filename: '1521631041972-the-importance-of-mockups.md' },
   { createdAt: 1521640519375, filename: '1521640519375-cultural-factors-in-design.md' },
   { createdAt: 1522827874762, filename: '1522827874762-another-stellar-review-on-clutch.md' },
@@ -16,6 +15,7 @@ const postsDatePair = [
   { createdAt: 1534505495576, filename: '1534505495576-mid-summer-2018-top-10-alexa-skills.md' },
   { createdAt: 1539840666250, filename: '1539840666250-best-smart-speakers-2018.md' },
   { createdAt: 1540623000000, filename: '1486461600000-product-management-through-the-eyes-of-a-small-ukrainian-company.md' },
+  { createdAt: 1540884600000, filename: '1517766794846-what-is-prototype-and-how-to-eat-it-in-a-right-way.md' },
 ];
 
 module.exports.postsDatePair = postsDatePair;
