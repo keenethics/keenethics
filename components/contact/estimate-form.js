515f5d8f390400b2ab9f37b2e0567860bdcf06c6
Dmytro Panontko <dmytro.panontko@keenethics.com>
2018-11-21 17:48:42 +0200

    WIP 21-11-18

---
+++ components/contact/estimate-form.js
@@ -0,0 +1,499 @@
+import 'whatwg-fetch';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const PointContainer = ({ point, children }) => (
+  <div className="estimate-point-container">
+    <p className="estimate-point-number">{point}</p>
+    <div className="estimate-point-content">{children}</div>
+  </div>
+);
+
+PointContainer.propTypes = {
+  point: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+};
+
+const PointContent = ({ head, children }) => (
+  <div className="point-content">
+    <p className="point-content-header">{head}</p>
+    <div className="point-content-body">{children}</div>
+  </div>
+);
+
+PointContent.propTypes = {
+  head: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+};
+
+class EstimateForm extends React.Component {
+  state = {
+    stage: {
+      value: 'New app',
+      error: false,
+    },
+    services: {
+      value: ['Web app'],
+      error: false,
+    },
+    pm: {
+      value: '',
+      error: false,
+    },
+    budget: {
+      value: 'I`m not sure',
+      error: false,
+    },
+    timeframe: {
+      value: 'I`m not sure',
+      error: false,
+    },
+    start: {
+      value: 'ASAP',
+      error: false,
+    },
+    phoneEstimate: {
+      value: '',
+      error: false,
+    },
+    emailEstimate: {
+      value: '',
+      error: false,
+    },
+    messageEstimate: {
+      value: '',
+      error: false,
+    },
+    name: {
+      value: '',
+      error: false,
+    },
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { updateState } = this.props;
+    updateState({ isPending: true });
+    fetch('/estimate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    }).then(response => response.json()).then((json) => {
+      const state = {
+        isPending: false,
+        status: json.status.toString(),
+      };
+      if (json && json.errorField) {
+        Object.assign(state, json.errorField);
+      }
+      this.setState(json.errorField);
+      updateState(state);
+    });
+  }
+  onChange = ({ target: { name, value, form: { services } } }) => {
+    const checkedCheckboxesValues = [...services]
+      .filter(({ checked }) => checked)
+      // eslint-disable-next-line no-shadow
+      .map(({ value }) => value);
+    if (name === 'services') {
+      this.setState({ services: { value: checkedCheckboxesValues } });
+    } else {
+      this.setState({
+        [name]: {
+          value,
+          error: '',
+        },
+      });
+    }
+  }
+
+  render = () => {
+    const {
+      emailEstimate,
+      name,
+      services,
+    } = this.state;
+
+    const { isPending, status } = this.props;
+
+    return (
+      <div className="estimate-form-container">
+        <form onSubmit={this.onSubmit} className="estimate-form">
+          <div className="estimate-title">let us Estimate your project</div>
+          <PointContainer point="01">
+            <PointContent
+              head="Stage"
+            >
+              <div className="estimate-input-cols">
+                <div className="input-radio-wrap">
+                  <input
+                    name="stage"
+                    type="radio"
+                    value="New app"
+                    id="new"
+                    onChange={this.onChange}
+                    checked={this.state.stage.value === 'New app'}
+                  />
+                  <label htmlFor="new" className="label-for-radio-btn">
+                    <p className="radio-lable-title">New app</p>
+                      to be built from scratch
+                  </label>
+                </div>
+                <div className="input-radio-wrap">
+                  <input
+                    name="stage"
+                    type="radio"
+                    value="Existing app"
+                    id="existing"
+                    onChange={this.onChange}
+                    checked={this.state.stage.value === 'Existing app'}
+                  />
+                  <label htmlFor="existing" className="label-for-radio-btn">
+                    <p className="radio-lable-title">Existing app</p>
+                      continue development
+                  </label>
+                </div>
+              </div>
+            </PointContent>
+          </PointContainer>
+          <PointContainer point="02">
+            <PointContent
+              head="What services are you interested in?"
+            >
+              <div className="estimate-input-cols">
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Web app"
+                    id="web"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Web app')}
+                  />
+                  <label htmlFor="web" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Web app</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="IOS app"
+                    id="ios"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('IOS app')}
+                  />
+                  <label htmlFor="ios" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>IOS app</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Linux app"
+                    id="linux"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Linux app')}
+                  />
+                  <label htmlFor="linux" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Linux app</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Server-side development"
+                    id="ssd"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Server-side development')}
+                  />
+                  <label htmlFor="ssd" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Server-side development</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Windows app"
+                    id="windows"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Windows app')}
+                  />
+                  <label htmlFor="windows" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Windows app</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="QA testing"
+                    id="qa"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('QA testing')}
+                  />
+                  <label htmlFor="qa" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>QA testing</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Android app"
+                    id="android"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Android app')}
+                  />
+                  <label htmlFor="android" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Android app</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="MacOS app"
+                    id="macos"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('MacOS app')}
+                  />
+                  <label htmlFor="macos" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>MacOS app</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Design"
+                    id="design"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Design')}
+                  />
+                  <label htmlFor="design" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Design</span>
+                  </label>
+                </div>
+                <div className="input-checkbox-wrap">
+                  <input
+                    name="services"
+                    type="checkbox"
+                    value="Other"
+                    id="other"
+                    onChange={this.onChange}
+                    className="check"
+                    checked={services.value.includes('Other')}
+                  />
+                  <label htmlFor="other" className="label-for-checkbox-btn">
+                    <span className={classnames('custom-checkbox', { error: services.error })}>
+                      <svg width="12px" height="10px" viewBox="0 0 12 10">
+                        <polyline points="1.5 6 4.5 9 10.5 1" />
+                      </svg>
+                    </span> <span>Other</span>
+                  </label>
+                </div>
+              </div>
+            </PointContent>
+          </PointContainer>
+          <PointContainer point="02">
+            <div className="question-title align-flex-start">
+              <input
+                name="pm"
+                type="checkbox"
+                value="PM/Product manager is required"
+                id="pm"
+                onChange={this.onChange}
+                className="check"
+              />
+              <label htmlFor="pm" className="label-for-checkbox-btn third-point">
+                <span className="pm-check">
+                  <svg width="12px" height="10px" viewBox="0 0 12 10">
+                    <polyline points="1.5 6 4.5 9 10.5 1" />
+                  </svg>
+                </span>
+                <span className="question-checkbox-title">Do you require PM/Product manager to save your time for tasks' description,
+                  tasks' assignment and tasks' assignment prioritizing?
+                </span>
+              </label>
+            </div>
+          </PointContainer>
+          <PointContainer point="04">
+            <PointContent
+              head="Expected budget"
+            >
+              <div className="estimate-input-cols">
+                <div className="input-select-wrap">
+                  <select name="budget" className="input-select" onChange={this.onChange}>
+                    <option value="I`m not sure">I`m not sure</option>
+                    <option value="Under $10000">Under $10000</option>
+                    <option value="$10000-$30000">$10000-$30000</option>
+                    <option value="$30000 and above">$30000 and above</option>
+                  </select>
+                </div>
+              </div>
+            </PointContent>
+          </PointContainer>
+          <PointContainer point="05">
+            <PointContent
+              head="Timeframe"
+            >
+              <div className="estimate-input-cols">
+                <div className="input-select-wrap">
+                  <select name="timeframe" className="input-select" onChange={this.onChange}>
+                    <option value="I`m not sure">I`m not sure</option>
+                    <option value="Less than 1 month">Less than 1 month</option>
+                    <option value="1 to 3 months">1 to 3 months</option>
+                    <option value="3 to 6 months">3 to 6 months</option>
+                    <option value="Above 6 months">Above 6 months</option>
+                  </select>
+                </div>
+              </div>
+            </PointContent>
+          </PointContainer>
+          <PointContainer point="06">
+            <PointContent
+              head="Start"
+            >
+              <div className="estimate-input-cols">
+                <div className="input-select-wrap">
+                  <select name="start" className="input-select" onChange={this.onChange}>
+                    <option value="ASAP">ASAP</option>
+                    <option value="in a couple of days">in a couple of days</option>
+                    <option value="in a week">in a week</option>
+                    <option value="in a couple of weeks">in a couple of weeks</option>
+                    <option value="in a month or later">in a month or later</option>
+                  </select>
+                </div>
+              </div>
+            </PointContent>
+          </PointContainer>
+          <div className="input-cols">
+            <div className="input-contacts">
+              <input
+                className={classnames({ error: name.error })}
+                name="name"
+                id="name"
+                type="text"
+                onChange={this.onChange}
+                required
+              />
+              <span className="highlight" />
+              <span className="bar" />
+              <label htmlFor="name">Name:</label>
+            </div>
+            <div className="input-contacts">
+              <input
+                name="phoneEstimate"
+                id="phoneEstimate"
+                type="tel"
+                onChange={this.onChange}
+                required
+              />
+              <span className="highlight" />
+              <span className="bar" />
+              <label htmlFor="phoneEstimate">Phone:</label>
+            </div>
+          </div>
+          <div className="input-cols">
+            <div className="input-contacts input-email">
+              <input
+                className={classnames({ error: emailEstimate.error })}
+                name="emailEstimate"
+                type="mail"
+                onChange={this.onChange}
+                required
+              />
+              <span className="highlight" />
+              <span className="bar" />
+              <label htmlFor="emailEstimate">Email:</label>
+            </div>
+          </div>
+          <div className="input-cols">
+            <div className="input-textarea">
+              <div className="input-textarea-title">Message:</div>
+              <textarea
+                name="messageEstimate"
+                placeholder="Write short description of your project or tell us your questions. Feel free to leave this blank and submit now - we will contact you and guide you through the process."
+                onChange={this.onChange}
+              />
+            </div>
+          </div>
+          <div className="form-footer">
+            <img src="/static/images/touch-screen.png" className="touch-screen" alt="touch-screen" />
+            <p className="form-footer-text">
+              Fill out additional information about your project and <span>get up to 50%</span> discount
+              for project specification’s elaboration - <a href="#">follow link</a>
+            </p>
+          </div>
+          {status && <div className="form-status">{status}</div>}
+          <div className="submit-btn">
+            <button
+              type="submit"
+              className={classnames('button button-send', { pending: isPending })}
+            >
+              <img src="/static/images/svg/send.svg" alt="send" />
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+EstimateForm.propTypes = {
+  isPending: PropTypes.bool.isRequired,
+  status: PropTypes.string.isRequired,
+  updateState: PropTypes.func.isRequired,
+};
+
+export default EstimateForm;
--- pages/contacts.js
+++ pages/contacts.js
@@ -2,10 +2,12 @@ import 'whatwg-fetch';
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import Layout from '../components/layout/main';
 import Background from '../components/content/background';
 import ContactForm from '../components/contact/contact-form';
+import EstimateForm from '../components/contact/estimate-form';
 
 export default class Contacts extends React.Component {
   constructor(props) {
@@ -34,11 +36,13 @@ export default class Contacts extends React.Component {
       },
       isPending: false,
       status: '',
+      isHelloForm: false, // TODO: set true
     };
 
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
   }
+
   onSubmit(e) {
     e.preventDefault();
 
@@ -65,6 +69,7 @@ export default class Contacts extends React.Component {
       this.setState(state);
     });
   }
+
   onChange({ target }) {
     this.setState({
       [target.name]: {
@@ -73,8 +78,14 @@ export default class Contacts extends React.Component {
       },
     });
   }
+
+  updateState = state => this.setState(state)
+
+  toggleForm = () => this.setState({ isHelloForm: !this.state.isHelloForm })
+
   render() {
     const { url } = this.props;
+    const { isHelloForm } = this.state;
 
     return (
       <Layout currentURL={url}>
@@ -85,33 +96,39 @@ export default class Contacts extends React.Component {
             </div>
             <div className="contacts-content" itemScope itemType="http://schema.org/Organization">
               <ul className="contacts-stars"><li /><li /><li /><li /></ul>
-              <ul className="company-info">
-                <li className="company-info-item" itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
-                  <a className="company-info-link" href="https://goo.gl/maps/eaAU8qqLZoo" rel="noopener noreferrer" target="_blank">
-                    <img width="15" src="/static/images/svg/con-map.svg" alt="" className="ico" />
-                    <p className="company-info-title" itemProp="streetAddress">Kulparkivska St, 59</p>
-                    <span className="company-info-subtitle"><span itemProp="addressLocality" style={{ display: 'inline' }}>Lviv</span>, <span itemProp="addressRegion" style={{ display: 'inline' }}>Ukraine</span></span>
-                  </a>
-                </li>
-                <li className="company-info-item">
-                  <a className="company-info-link" href="tel:+380968147266">
-                    <img width="15" src="/static/images/svg/con-tel.svg" alt="" className="ico" />
-                    <p className="company-info-title" itemProp="telephone">+38 (096) 814 72 66</p>
-                    <span className="company-info-subtitle">Give Us a Call</span>
-                  </a>
-                </li>
-                <li className="company-info-item">
-                  <a className="company-info-link" href="mailto:founders@keenethics.com">
-                    <img width="15" src="/static/images/svg/con-mail.svg" alt="" className="ico" />
-                    <p className="company-info-title">founders@keenethics.com</p>
-                    <span className="company-info-subtitle">Drop Us a Letter</span>
-                  </a>
-                </li>
-              </ul>
+              <div className="contacts-content-left">
+                <div className="contacts-buttons">
+                  <button className={classnames('contacts-button-hello', { disabled: !isHelloForm })} onClick={this.toggleForm}>Say hello</button>
+                  <button className={classnames('contacts-button-estimate', { disabled: isHelloForm })} onClick={this.toggleForm}>Estimate your project</button>
+                </div>
+                <ul className="company-info">
+                  <li className="company-info-item" itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
+                    <a className="company-info-link" href="https://goo.gl/maps/eaAU8qqLZoo" rel="noopener noreferrer" target="_blank">
+                      <img width="15" src="/static/images/svg/con-map.svg" alt="" className="ico" />
+                      <p className="company-info-title" itemProp="streetAddress">Kulparkivska St, 59</p>
+                      <span className="company-info-subtitle"><span itemProp="addressLocality" style={{ display: 'inline' }}>Lviv</span>, <span itemProp="addressRegion" style={{ display: 'inline' }}>Ukraine</span></span>
+                    </a>
+                  </li>
+                  <li className="company-info-item">
+                    <a className="company-info-link" href="tel:+380968147266">
+                      <img width="15" src="/static/images/svg/con-tel.svg" alt="" className="ico" />
+                      <p className="company-info-title" itemProp="telephone">+38 (096) 814 72 66</p>
+                      <span className="company-info-subtitle">Give Us a Call</span>
+                    </a>
+                  </li>
+                  <li className="company-info-item">
+                    <a className="company-info-link" href="mailto:founders@keenethics.com">
+                      <img width="15" src="/static/images/svg/con-mail.svg" alt="" className="ico" />
+                      <p className="company-info-title">founders@keenethics.com</p>
+                      <span className="company-info-subtitle">Drop Us a Letter</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
               <div className="contacts-form-container">
                 <div className="contacts-form-background" />
                 <div className="contacts-mail" />
-                <ContactForm />
+                {isHelloForm ? <ContactForm updateState={this.updateState} /> : <EstimateForm />}
               </div>
             </div>
           </div>
