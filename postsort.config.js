f372fafa4ba2aa5bd9aaf78139f9e17ef9e69377
Max Soloviov <max.soloviov@keenethics.com>
2018-11-28 09:50:17 +0200

    Added 'Your Data is Safe in Ukraine' article

--- postsort.config.js
+++ postsort.config.js
@@ -18,6 +18,7 @@ const postsDatePair = [
   { createdAt: 1540884600000, filename: '1517766794846-what-is-prototype-and-how-to-eat-it-in-a-right-way.md' },
   { createdAt: 1542178800000, filename: '1542178800000-best-amazon-alexa-skills-mid-autumn-2018.md' },
   { createdAt: 1542956400000, filename: '1542956400000-from-partners-to-friends-with-worldlabs.md' },
+  { createdAt: 1543388400000, filename: '1543388400000-your-data-is-safe-in-ukraine.md' },
 ];
 
 module.exports.postsDatePair = postsDatePair;
