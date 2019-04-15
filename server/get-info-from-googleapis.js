72c7a1f1f4906ec6247e97b12d66f1cf55af5c33
Dmytro Panontko <dmytro.panontko@keenethics.com>
2019-04-15 18:17:24 +0300

    Refactor 'get-info-from-googleapis'

--- server/get-info-from-googleapis.js
+++ server/get-info-from-googleapis.js
@@ -1,20 +1,18 @@
 const { google } = require('googleapis');
 const { spreadSheet } = require('./config');
 const _ = require('lodash');
+const memoize = require('lodash/memoize');
 
 const TEAM_SHEET_ID = spreadSheet.id;
 const CREDS_FILE = spreadSheet.securityFile;
-const scopes = ['https://www.googleapis.com/auth/drive.readonly'];
+const googleScopes = ['https://www.googleapis.com/auth/drive.readonly'];
 const TEAM_RANGE = spreadSheet.teamRange;
 const CAREERS_RANGE = spreadSheet.careersRange;
 
 const ranges = [TEAM_RANGE, CAREERS_RANGE];
 
+const TEN_MINUTES = 10 * 60 * 1000;
 const TITLE_COLUMN_INDEX = 0;
-
-let dataStore = null;
-let updateTime = null;
-const timeToLive = 10 * 60 * 1000; // 10min
 
 const arrayOfArraysToCollection = (arr) => {
   const properties = arr[TITLE_COLUMN_INDEX];
@@ -22,69 +20,70 @@ const arrayOfArraysToCollection = (arr) => {
     .map(v => _.fromPairs(properties.map((property, index) => ([property, v[index]]))));
 };
 
-const isValidData = () => {
-  if (!dataStore || !updateTime) {
-    return false;
-  }
-
-  const diffTime = +new Date() - updateTime;
-  return diffTime < timeToLive;
-};
-
-const updateData = (data) => {
-  dataStore = data;
-  updateTime = +new Date();
-};
-
 const getSheetValues = (valueRanges, rangeName) =>
   valueRanges.find(v => new RegExp(rangeName).test(v.range)).values;
 
-const getData = async () => {
-  if (isValidData()) {
-    return Promise.resolve(dataStore);
-  }
+const getClient = async (keyFile, scopes) => google.auth.getClient({
+  keyFile,
+  scopes,
+});
 
+class CustomMap extends Map {
+  async get(key) {
+    if (!super.has(key)) {
+      return;
+    }
+
+    const result = await super.get(key);
+
+    // eslint-disable-next-line
+    if ((Date.now() - result._date) >= TEN_MINUTES) {
+      super.delete(key);
+    }
+
+    // eslint-disable-next-line
+    return result;
+  }
+}
+
+memoize.Cache = CustomMap;
+
+const getSheets = memoize(async (spreadsheetId) => { // TEAM_SHEET_ID
+  const client = await getClient(CREDS_FILE, googleScopes);
+
+  const sheets = google.sheets('v4');
+
+  const table = await sheets.spreadsheets.values.batchGet({
+    auth: client,
+    spreadsheetId,
+    ranges,
+  });
+
+  const result = _.get(table, 'data', {});
+  // eslint-disable-next-line
+  result._date = Date.now();
+
+  return result;
+});
+
+const getData = async (rangeName, sheetId) => {
   try {
-    const client = await google.auth.getClient({
-      keyFile: CREDS_FILE,
-      scopes,
-    });
+    const sheetData = await getSheets(sheetId);
 
-    const sheets = google.sheets('v4');
+    const team = getSheetValues(sheetData.valueRanges, rangeName);
 
-    const table = await sheets.spreadsheets.values.batchGet({
-      auth: client,
-      spreadsheetId: TEAM_SHEET_ID,
-      ranges,
-    });
+    return arrayOfArraysToCollection(team);
+  } catch (err) {
+    console.error(err);
 
-
-    const team = getSheetValues(table.data.valueRanges, TEAM_RANGE);
-    const careers = getSheetValues(table.data.valueRanges, CAREERS_RANGE);
-
-    const data = {
-      team: arrayOfArraysToCollection(team),
-      careers: arrayOfArraysToCollection(careers),
-    };
-
-    updateData(data);
-    return Promise.resolve(dataStore);
-  } catch (e) {
-    console.error(e);
-    return null;
+    return [];
   }
 };
 
-getData();
+getData(TEAM_RANGE, TEAM_SHEET_ID);
 
-const getTeam = async () => {
-  const d = await getData();
-  return d.team;
-};
-const getCareers = async () => {
-  const d = await getData();
-  return d.careers;
-};
+const getTeam = async () => getData(TEAM_RANGE, TEAM_SHEET_ID);
+const getCareers = async () => getData(CAREERS_RANGE, TEAM_SHEET_ID);
 
 module.exports = {
   getTeam,
