036bfa45b8c0618587dc790009cdd31f1d20dba8
antonmorya <antonmorya@gmail.com>
2019-07-26 12:39:24 +0300

    next.config edits po tush new env. var.

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = withSass({
       }
     }
 
-    config.plugins.push(new webpack.EnvironmentPlugin(['REACT_APP_SPACE_ID', 'REACT_APP_ACCESS_TOKEN']))
+    config.plugins.push(new webpack.EnvironmentPlugin(['REACT_APP_SPACE_ID', 'REACT_APP_ACCESS_TOKEN', 'REACT_APP_PREVIEW_TOKEN']))
 
     return config;
   },
