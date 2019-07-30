57c59c915ea8c0b967d0acd13195e50c675c0ceb
antonmorya <antonmorya@gmail.com>
2019-07-30 13:55:31 +0300

    Contacts component refactored

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,6 +1,6 @@
 import { withRouter } from 'next/router';
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -10,146 +10,147 @@ import EstimateForm from '../components/contacts/estimate-form';
 import ContactForm from '../components/contacts/contact-form';
 import Notify from '../components/notify/notify';
 
-class Contacts extends React.Component {
-  constructor(props) {
-    super(props);
-    const { query } = props.router;
+let ContactUsContext;
+const { Provider, Consumer } = (ContactUsContext = React.createContext());
 
-    this.state = {
-      isPending: false,
-      status: '',
-      activeContactForm: query.activeForm !== 'estimate',
-      notifyIsVisible: false,
-      notifyMessage: null,
-    };
-    this.onClick = this.onClick.bind(this);
+const Contacts = ({ router }) => {
+  const { query } = router;
+
+  const [isPending, setIsPending] = useState(false);
+  const [status, setStatus] = useState('');
+  const [activeContactForm, setActiveContactForm] = useState(query.activeForm !== 'estimate');
+  const [notifyIsVisible, setNotifyIsVisible] = useState(false);
+  const [notifyMessage, setNotifyMessage] = useState(null);
+
+  function onClick({ target }) {
+    setActiveContactForm(target.name === 'contact-form-btn');
   }
 
-  onClick({ target }) {
-    this.setState({
-      activeContactForm: target.name === 'contact-form-btn',
-    });
-  }
-
-  render() {
-    const {
-      activeContactForm, isPending, status, notifyIsVisible, notifyMessage,
-    } = this.state;
-
-    return (
-      <Layout>
-        {notifyIsVisible && (
-          <Notify notifyMessage={notifyMessage} updateState={state => this.setState(state)} />
-        )}
-        <div className="contacts-page">
-          <div className="contacts-socket">
-            <div className="title-page">
-              <h1 className="title">Contact Us</h1>
-            </div>
-            <div
-              className={`contact-us-form-wrapper ${
-                activeContactForm ? 'contacts-block' : 'estimate-block-background'
-              }`}
-              itemScope
-              itemType="http://schema.org/Organization"
-            >
-              <address>
-                <ul className="contacts-list">
-                  <li itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
-                    <a
-                      href="https://goo.gl/maps/eaAU8qqLZoo"
-                      rel="noopener noreferrer nofollow"
-                      target="_blank"
-                    >
-                      <img
-                        width="15"
-                        src="/static/images/svg/con-map.svg"
-                        alt="location"
-                        className="ico"
-                      />
-                      <div itemProp="streetAddress">Kulparkivska St, 59</div>
-                      <span>
-                        <span itemProp="addressLocality" style={{ display: 'inline' }}>
-                          Lviv
-                        </span>
-                        ,&nbsp;
-                        <span itemProp="addressRegion" style={{ display: 'inline' }}>
-                          Ukraine
-                        </span>
+  return (
+    <Layout>
+      {notifyIsVisible && (
+        <Notify
+          notifyMessage={notifyMessage}
+          setNotifyIsVisible={setNotifyIsVisible}
+          setNotifyMessage={setNotifyMessage}
+        />
+      )}
+      <div className="contacts-page">
+        <div className="contacts-socket">
+          <div className="title-page">
+            <h1 className="title">Contact Us</h1>
+          </div>
+          <div
+            className={`contact-us-form-wrapper ${
+              activeContactForm ? 'contacts-block' : 'estimate-block-background'
+            }`}
+            itemScope
+            itemType="http://schema.org/Organization"
+          >
+            <address>
+              <ul className="contacts-list">
+                <li itemProp="address" itemScope itemType="http://schema.org/PostalAddress">
+                  <a
+                    href="https://goo.gl/maps/eaAU8qqLZoo"
+                    rel="noopener noreferrer nofollow"
+                    target="_blank"
+                  >
+                    <img
+                      width="15"
+                      src="/static/images/svg/con-map.svg"
+                      alt="location"
+                      className="ico"
+                    />
+                    <div itemProp="streetAddress">Kulparkivska St, 59</div>
+                    <span>
+                      <span itemProp="addressLocality" style={{ display: 'inline' }}>
+                        Lviv
                       </span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="tel:+380968147266">
-                      <img
-                        width="15"
-                        src="/static/images/svg/con-tel.svg"
-                        alt="Call"
-                        className="ico"
-                      />
-                      <div itemProp="telephone">+38 (096) 814 72 66</div>
-                      <span>Give Us a Call</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="mailto:business@keenethics.com">
-                      <img
-                        width="15"
-                        src="/static/images/svg/con-mail.svg"
-                        alt="email"
-                        className="ico"
-                      />
-                      <div>business@keenethics.com</div>
-                      <span>Drop Us a Letter</span>
-                    </a>
-                  </li>
-                </ul>
-              </address>
-              <div className="active-form-wrapper">
-                <div className="btn-group">
-                  <button
-                    onClick={this.onClick}
-                    name="contact-form-btn"
-                    className={classnames('button contacts-form-btn contact-form-btn', {
-                      disabled: !activeContactForm,
-                    })}
-                    type="button"
-                  >
-                    Say Hello
-                  </button>
-                  <button
-                    onClick={this.onClick}
-                    name="estimate-form-btn"
-                    className={classnames('button contacts-form-btn estimate-form-btn', {
-                      disabled: activeContactForm,
-                    })}
-                    type="button"
-                  >
-                    Estimate your project
-                  </button>
-                </div>
-                {this.state.activeContactForm ? (
-                  <ContactForm
-                    isPending={isPending}
-                    status={status}
-                    updateState={state => this.setState(state)}
-                  />
+                      ,&nbsp;
+                      <span itemProp="addressRegion" style={{ display: 'inline' }}>
+                        Ukraine
+                      </span>
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="tel:+380968147266">
+                    <img
+                      width="15"
+                      src="/static/images/svg/con-tel.svg"
+                      alt="Call"
+                      className="ico"
+                    />
+                    <div itemProp="telephone">+38 (096) 814 72 66</div>
+                    <span>Give Us a Call</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:business@keenethics.com">
+                    <img
+                      width="15"
+                      src="/static/images/svg/con-mail.svg"
+                      alt="email"
+                      className="ico"
+                    />
+                    <div>business@keenethics.com</div>
+                    <span>Drop Us a Letter</span>
+                  </a>
+                </li>
+              </ul>
+            </address>
+            <div className="active-form-wrapper">
+              <div className="btn-group">
+                <button
+                  onClick={onClick}
+                  name="contact-form-btn"
+                  className={classnames('button contacts-form-btn contact-form-btn', {
+                    disabled: !activeContactForm,
+                  })}
+                  type="button"
+                >
+                  Say Hello
+                </button>
+                <button
+                  onClick={onClick}
+                  name="estimate-form-btn"
+                  className={classnames('button contacts-form-btn estimate-form-btn', {
+                    disabled: activeContactForm,
+                  })}
+                  type="button"
+                >
+                  Estimate your project
+                </button>
+              </div>
+              <Provider
+                value={{
+                  isPending,
+                  setIsPending,
+                  status,
+                  setStatus,
+                  setNotifyIsVisible,
+                  notifyMessage,
+                  setNotifyMessage,
+                }}
+              >
+                {activeContactForm ? (
+                  <ContactForm />
                 ) : (
                   <EstimateForm
                     isPending={isPending}
                     status={status}
-                    updateState={state => this.setState(state)}
+                    // updateState={state => this.setState(state)}
                   />
                 )}
-              </div>
+              </Provider>
             </div>
           </div>
-          <Background />
         </div>
-      </Layout>
-    );
-  }
-}
+        <Background />
+      </div>
+    </Layout>
+  );
+};
 
 Contacts.propTypes = {
   router: PropTypes.object,
@@ -159,3 +160,5 @@ Contacts.defaultProps = {
 };
 
 export default withRouter(Contacts);
+
+export { ContactUsContext };
