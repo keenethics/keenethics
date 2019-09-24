16628be8494af085178b3c0cfc36047e580f396c
antonmorya <antonmorya@gmail.com>
2019-09-24 15:55:16 +0300

    Posts tiles right margins edits

--- components/blog/posts.js
+++ components/blog/posts.js
@@ -33,73 +33,75 @@ const Posts = ({ posts }) => {
   }, []);
 
   return (
-    <div className="blog-page-posts">
+    <React.Fragment>
       <Modal open={modalOpen} onClose={() => setModalOpen(false)} />
-      {posts.map((post, counter) => {
-        const {
-          fields: { slug, title, heroImage, publishDate, tags, description }
-        } = post;
+      <div className="blog-page-posts">
+        {posts.map((post, counter) => {
+          const {
+            fields: { slug, title, heroImage, publishDate, tags, description }
+          } = post;
 
-        const url = _.get(heroImage, "fields.file.url");
-        const styles =
-          counter === 0 && !isMobileWindowMode
-            ? {
-                backgroundImage: url
-                  ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=1200)`
-                  : ""
-              }
-            : {
-                backgroundImage: url
-                  ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=${
-                      isMobileWindowMode ? 700 : 950
-                    })`
-                  : ""
-              };
-        /* const alt =
+          const url = _.get(heroImage, "fields.file.url");
+          const styles =
+            counter === 0 && !isMobileWindowMode
+              ? {
+                  backgroundImage: url
+                    ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=1200)`
+                    : ""
+                }
+              : {
+                  backgroundImage: url
+                    ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=${
+                        isMobileWindowMode ? 700 : 950
+                      })`
+                    : ""
+                };
+          /* const alt =
         _.get(heroImage, "fields.description") ||
         _.get(heroImage, "fields.title"); */
 
-        return (
-          <div key={slug} className="blog-page-post">
-            <div className="blog-page-post-wrap-img">
-              <a className="" href={`/post?name=${slug}`}>
-                <div className="blog-page-post-img" style={styles} />
-              </a>
-            </div>
-            <div className="blog-page-post-info">
-              <div className="blog-page-post-info-tag-container">
-                {tags.map(tag => (
-                  <span className="tag" key={tag}>
-                    {tag}
-                  </span>
-                ))}
-              </div>
-              <span className="blog-page-post-date">
-                <Moment format="MMMM D, YYYY">{publishDate}</Moment>
-              </span>
-              <h2>
+          return (
+            <div key={slug} className="blog-page-post">
+              <div className="blog-page-post-wrap-img">
                 <a className="" href={`/post?name=${slug}`}>
-                  {title}
+                  <div className="blog-page-post-img" style={styles} />
                 </a>
-              </h2>
-              {counter === 0 && description && (
-                <React.Fragment>
-                  <p className="blog-page-post-description">
-                    {description.slice(0, 128).concat("...")}
-                  </p>
-                  <a
-                    className="blog-page-post-read-more"
-                    href={`/post?name=${slug}`}
-                  >
-                    Read More
+              </div>
+              <div className="blog-page-post-info">
+                <div className="blog-page-post-info-tag-container">
+                  {tags.map(tag => (
+                    <span className="tag" key={tag}>
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <span className="blog-page-post-date">
+                  <Moment format="MMMM D, YYYY">{publishDate}</Moment>
+                </span>
+                <h2>
+                  <a className="" href={`/post?name=${slug}`}>
+                    {title}
                   </a>
-                </React.Fragment>
-              )}
+                </h2>
+                {counter === 0 && description && (
+                  <React.Fragment>
+                    <p className="blog-page-post-description">
+                      {description.slice(0, 128).concat("...")}
+                    </p>
+                    <a
+                      className="blog-page-post-read-more"
+                      href={`/post?name=${slug}`}
+                    >
+                      Read More
+                    </a>
+                  </React.Fragment>
+                )}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 };
 
