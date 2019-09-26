d2e66636db7540d7cf092c9005389a8f7d99ee16
antonmorya <antonmorya@gmail.com>
2019-09-26 16:09:02 +0300

    Subscribe modal appears on click

--- components/blog/posts.js
+++ components/blog/posts.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
-import Modal from "../subscribeModal";
 
 const _ = require("lodash");
 
 const Posts = ({ posts }) => {
-  const [modalOpen, setModalOpen] = useState(false);
   const [windowMode, setWindowMode] = useState("desktop");
   const isMobileWindowMode = windowMode === "mobile";
 
@@ -22,86 +20,78 @@ const Posts = ({ posts }) => {
     windowModeSetter();
     window.addEventListener("resize", windowModeSetter);
 
-    const timer = setTimeout(function() {
-      setModalOpen(true);
-    }, 30000);
-
     return () => {
       window.removeEventListener("resize", windowModeSetter);
-      clearTimeout(timer);
     };
   }, []);
 
   return (
-    <React.Fragment>
-      <Modal open={modalOpen} onClose={() => setModalOpen(false)} />
-      <div className="blog-page-posts">
-        {posts.map((post, counter) => {
-          const {
-            fields: { slug, title, heroImage, publishDate, tags, description }
-          } = post;
+    <div className="blog-page-posts">
+      {posts.map((post, counter) => {
+        const {
+          fields: { slug, title, heroImage, publishDate, tags, description }
+        } = post;
 
-          const url = _.get(heroImage, "fields.file.url");
-          const styles =
-            counter === 0 && !isMobileWindowMode
-              ? {
-                  backgroundImage: url
-                    ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=1200)`
-                    : ""
-                }
-              : {
-                  backgroundImage: url
-                    ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=${
-                        isMobileWindowMode ? 700 : 950
-                      })`
-                    : ""
-                };
-          /* const alt =
+        const url = _.get(heroImage, "fields.file.url");
+        const styles =
+          counter === 0 && !isMobileWindowMode
+            ? {
+                backgroundImage: url
+                  ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=1200)`
+                  : ""
+              }
+            : {
+                backgroundImage: url
+                  ? `url(https://${url}?fm=jpg&fl=progressive&q=85&w=${
+                      isMobileWindowMode ? 700 : 950
+                    })`
+                  : ""
+              };
+        /* const alt =
         _.get(heroImage, "fields.description") ||
         _.get(heroImage, "fields.title"); */
 
-          return (
-            <div key={slug} className="blog-page-post">
-              <div className="blog-page-post-wrap-img">
-                <a className="" href={`/post?name=${slug}`}>
-                  <div className="blog-page-post-img" style={styles} />
-                </a>
-              </div>
-              <div className="blog-page-post-info">
-                <div className="blog-page-post-info-tag-container">
-                  {tags.map(tag => (
-                    <span className="tag" key={tag}>
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-                <span className="blog-page-post-date">
-                  <Moment format="MMMM D, YYYY">{publishDate}</Moment>
-                </span>
-                <h2>
-                  <a className="" href={`/post?name=${slug}`}>
-                    {title}
-                  </a>
-                </h2>
-                {counter === 0 && description && (
-                  <React.Fragment>
-                    <p className="blog-page-post-description">
-                      {description.slice(0, 128).concat("...")}
-                    </p>
-                    <a
-                      className="blog-page-post-read-more"
-                      href={`/post?name=${slug}`}
-                    >
-                      Read More
-                    </a>
-                  </React.Fragment>
-                )}
-              </div>
+        return (
+          <div key={slug} className="blog-page-post">
+            <div className="blog-page-post-wrap-img">
+              <a className="" href={`/post?name=${slug}`}>
+                <div className="blog-page-post-img" style={styles} />
+              </a>
             </div>
-          );
-        })}
-      </div>
-    </React.Fragment>
+            <div className="blog-page-post-info">
+              <div className="blog-page-post-info-tag-container">
+                {tags.map(tag => (
+                  <span className="tag" key={tag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <span className="blog-page-post-date">
+                <Moment format="MMMM D, YYYY">{publishDate}</Moment>
+              </span>
+              <h2>
+                <a className="" href={`/post?name=${slug}`}>
+                  {title}
+                </a>
+              </h2>
+              {counter === 0 && description && (
+                <React.Fragment>
+                  <p className="blog-page-post-description">
+                    {description.slice(0, 128).concat("...")}
+                  </p>
+                  <a
+                    className="blog-page-post-read-more"
+                    href={`/post?name=${slug}`}
+                  >
+                    Read More
+                  </a>
+                </React.Fragment>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
