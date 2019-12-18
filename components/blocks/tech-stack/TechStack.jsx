7bef25a569b7d4e065f734b4053e67dcfe4076f9
Gornostaev Sergey <svgornostaev@gmail.com>
2019-12-18 23:23:40 +0200

    Fix blocks

--- components/blocks/tech-stack/TechStack.jsx
+++ components/blocks/tech-stack/TechStack.jsx
@@ -10,79 +10,81 @@ const TechStack = () => (
         These are the tools we use to reach the optimal software solution.
       </div>
     </header>
-    <div className="tech-stack">
-      <div className="tech-stack--block">
-        <div className="tech-stack--block-title">
-          JavaScript
-        </div>
-        <div className="tech-stack--block-list">
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/nodejs.svg" alt="node.js" />
+    <div className="block--content">
+      <div className="tech-stack">
+        <div className="tech-stack--block">
+          <div className="tech-stack--block-title">
+            JavaScript
           </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/angular.svg" alt="angular" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/react-native.svg" alt="react native" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/tech.svg" alt="tech" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/vue.svg" alt="vue" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/express.svg" alt="express" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/tech2.svg" alt="tech2" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/cordova.svg" alt="cordova" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/phonegap.svg" alt="phonegap" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/svelte.svg" alt="svelte" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/meteor.svg" alt="meteor" />
-          </div>
-        </div>
-      </div>
-      <div className="tech-stack--block">
-        <div className="tech-stack--block-title">
-          Databases
-        </div>
-        <div className="tech-stack--block-list">
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/mongodb.svg" alt="mongodb" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/mysql.svg" alt="mysql" />
+          <div className="tech-stack--block-list">
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/nodejs.svg" alt="node.js" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/angular.svg" alt="angular" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/react-native.svg" alt="react native" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/tech.svg" alt="tech" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/vue.svg" alt="vue" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/express.svg" alt="express" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/tech2.svg" alt="tech2" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/cordova.svg" alt="cordova" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/phonegap.svg" alt="phonegap" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/svelte.svg" alt="svelte" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/meteor.svg" alt="meteor" />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="tech-stack--block">
-        <div className="tech-stack--block-title">
-          API Integration
+        <div className="tech-stack--block">
+          <div className="tech-stack--block-title">
+            Databases
+          </div>
+          <div className="tech-stack--block-list">
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/mongodb.svg" alt="mongodb" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/mysql.svg" alt="mysql" />
+            </div>
+          </div>
         </div>
-        <div className="tech-stack--block-list">
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/rest.svg" alt="rest" />
+        <div className="tech-stack--block">
+          <div className="tech-stack--block-title">
+            API Integration
           </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/paypal.svg" alt="paypal" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/graphql.svg" alt="graphql" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/braintree.svg" alt="braintree" />
-          </div>
-          <div className="tech-stack--block-item">
-            <img src="/static/main-blocks/tech-stack/stripe.svg" alt="stripe" />
+          <div className="tech-stack--block-list">
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/rest.svg" alt="rest" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/paypal.svg" alt="paypal" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/graphql.svg" alt="graphql" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/braintree.svg" alt="braintree" />
+            </div>
+            <div className="tech-stack--block-item">
+              <img src="/static/main-blocks/tech-stack/stripe.svg" alt="stripe" />
+            </div>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Link from 'next/link';
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 
 import Layout from '../components/layout/main';
+import Main from '../components/blocks/main/Main';
 import OurMethods from '../components/blocks/our-methods/OurMethods';
 import Industries from '../components/blocks/industries/Industries';
 import TechStack from '../components/blocks/tech-stack/TechStack';
-import SocialButton from '../components/social-buttons/main';
 import OurServices from './our-services';
 import Founders from './founders';
 import Projects from './home-page-projects';
@@ -53,62 +50,45 @@ export default class Index extends React.Component {
   constructor(props) {
     super(props);
 
-    const sections = ['top', 'our-methods', 'services', 'founders', 'projects'];
-    this.state = {
-      sections,
-      currentSection: 'top',
-      nextSection: 'services',
-      isInit: true,
-      isMobile: false,
-      // isDesktop: false,
-      showCircleAnimation: false,
-      fpAPI: null,
-      isFPDestroyed: false,
-    };
+    this.state = {};
 
-    this.updateDimensions = this.updateDimensions.bind(this);
-    this.scrollClick = this.scrollClick.bind(this);
     // this.setPostsContext = this.setPostsContext.bind(this);
     // this.getPosts = this.getPosts.bind(this);
   }
 
-  componentDidMount() {
-    // window.addEventListener('resize', (e) => {
-    //   const {
-    //     isMobile,
-    //     // isDesktop,
-    //   } = this.state;
-    //   if (!isMobile && e.target.innerWidth < 768) {
-    //     this.setState({
-    //       isMobile: true,
-    //       isFPDestroyed: true,
-    //     });
-    //     return;
-    //   }
-    //   if (isMobile && e.target.innerWidth >= 768) {
-    //     this.setState({
-    //       isMobile: false,
-    //       isFPDestroyed: false,
-    //     });
-    //   }
-    // if (isDesktop && e.target.innerWidth <= 1024) {
-    //   this.setState({
-    //     isDesktop: false,
-    //   });
-    // }
-    // if (!isDesktop && e.target.innerWidth >= 1024) {
-    //   this.setState({
-    //     isDesktop: true,
-    //   });
-    // }
-    // });
-    // this.getPosts();
-    // this.setState({ isMobile: window.innerWidth <= 768 });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', () => {});
-  }
+  // componentDidMount() {
+  // window.addEventListener('resize', (e) => {
+  //   const {
+  //     isMobile,
+  //     // isDesktop,
+  //   } = this.state;
+  //   if (!isMobile && e.target.innerWidth < 768) {
+  //     this.setState({
+  //       isMobile: true,
+  //       isFPDestroyed: true,
+  //     });
+  //     return;
+  //   }
+  //   if (isMobile && e.target.innerWidth >= 768) {
+  //     this.setState({
+  //       isMobile: false,
+  //       isFPDestroyed: false,
+  //     });
+  //   }
+  // if (isDesktop && e.target.innerWidth <= 1024) {
+  //   this.setState({
+  //     isDesktop: false,
+  //   });
+  // }
+  // if (!isDesktop && e.target.innerWidth >= 1024) {
+  //   this.setState({
+  //     isDesktop: true,
+  //   });
+  // }
+  // });
+  // this.getPosts();
+  // this.setState({ isMobile: window.innerWidth <= 768 });
+  // }
 
   static async getInitialProps() {
     const contResp = await getPostsList();
@@ -122,151 +102,26 @@ export default class Index extends React.Component {
   //   setPostsContext(posts);
   // };
 
-  scrollClick() {
-    const { fpAPI } = this.state;
-    fpAPI.moveTo(2);
-  }
-
-  updateDimensions(e) {
-    const { isMobile } = this.state;
-    if (!isMobile && e.target.innerWidth < 768) {
-      this.setState({ isMobile: true });
-    }
-    if (isMobile && e.target.innerWidth >= 768) {
-      this.setState({ isMobile: false });
-    }
-  }
-
   render() {
-    // setTimeout(() => {
-    //   const w = window;
-    //   if (w) {
-    //     const elem = w.document ? w.document.getElementById('__next') : null;
-    //     if (elem) elem.classList.add('testloader');
-    //   }
-    // }, 1000);
-    const {
-      sections,
-      currentSection,
-      nextSection,
-      isInit,
-      showCircleAnimation,
-      isMobile,
-      isFPDestroyed,
-      // isDesktop,
-    } = this.state;
     const { posts } = this.props;
-    const isFirstScroll = nextSection === sections[1] && !isInit && !isFPDestroyed;
+
     return (
       <Layout>
         <JsonLd data={companyData} />
-        <div className="section" id={sections[0]}>
-          <div className={classnames('home-page')}>
-            <div className="home-page-content">
-              <div className="social-icons home-page-social">
-                <SocialButton />
-              </div>
-              <h4 className={classnames('home-page-small-title', {
-                faded: isFirstScroll,
-              })}
-              >
-                Keen &amp; Ethical Software
-                <br />
-                Development
-              </h4>
-              <div className={classnames('vertical-line', {
-                faded: isFirstScroll,
-              })}
-              />
-              <h1 className={classnames('home-page-large-title', {
-                faded: isFirstScroll,
-              })}
-              >
-                Full-cycle
-                <br />
-                web and mobile
-                <br />
-                development partner
-              </h1>
-              <div className="home-page-rates-container">
-                <div className="home-page-content-link">
-                  <a
-                    href="https://www.upwork.com/agencies/~0106b5437592391f94"
-                    className={classnames('link link-upwork', { 'link-upwork-white faded': isFirstScroll })}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                  >
-                    Top rated Upwork agency
-                  </a>
-                </div>
-                <div className="home-page-content-link">
-                  <a
-                    href="https://clutch.co/profile/keenethics"
-                    className={classnames('link link-clutch', { 'link-clutch-white faded': isFirstScroll })}
-                    target="_blank"
-                    rel="noreferrer noopener nofollow"
-                  >
-                    5-stars rated company at Clutch
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className={classnames('home-page-side-info')}>
-              <div className="side-info-bg">
-                <div className="home-page-side-info__image" />
-                <div className="home-page-side-info__image" />
-                <div className="home-page-side-info__image" />
-              </div>
-              <div className="circle-container">
-                <div className={classnames('orange-circle', { 'circle-anim': showCircleAnimation })}>
-                  <div className="border" />
-                </div>
-              </div>
-            </div>
-            <div className={classnames('side-info-top', { faded: isFirstScroll })}>
-              <Link href="/contacts?activeForm=estimate">
-                <a className="button contacts-goal side-info-top-content">Free estimate</a>
-              </Link>
-            </div>
-            <div className="scroll-info-container">
-              <p className="vertical-text">scroll down to see more</p>
-              <button className="mouse-scroll" type="button" onClick={() => this.scrollClick()}>
-                <span className="mouse-scroll-wheel" />
-              </button>
-            </div>
-            <div className={classnames('bg-scroll', { 'bg-scroll-animate': currentSection === sections[0] && !isInit })} />
-          </div>
-        </div>
-        <OurServices
-          section={sections[1]}
-          show={sections[1] === nextSection && !isInit}
-          isMobile={isMobile}
-        />
+        <Main />
+        <OurServices />
         <OurMethods />
         <Industries />
-        <Founders
-          section={sections[2]}
-          isMobile={isMobile}
-        />
-        <Projects
-          section={sections[3]}
-          isMobile={isMobile}
-        />
+        <Founders />
+        <Projects />
         <TechStack />
         <PostsContext.Provider value={posts}>
           <Blog
-            section={sections[4]}
             posts={posts}
             setPostsContext={this.setPostsContext}
           />
-          <LetsStart
-            section={sections[5]}
-          />
-          <HomeFooter
-            section={sections[6]}
-            isMobile={isMobile}
-            // isDesktop={isDesktop}
-          />
+          <LetsStart />
+          <HomeFooter />
         </PostsContext.Provider>
       </Layout>
     );
