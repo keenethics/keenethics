d01ed5da2d6f9f510bab4f28923da913f77ec33d
Vitalii Melnychenko <vitaliy.melnychenko@keenethics.com>
2019-12-23 12:56:22 +0200

    Code refactoring

--- components/contacts/estimate-form.js
+++ components/contacts/estimate-form.js
@@ -940,10 +940,11 @@ Start
                 allowedExts=".pdf, doc, docx, jpeg, jpg, png, xls, xlsx, ppt, pptx"
                 onChange={
                   (e) => {
-                    if ((e.target.files[0] || {}).name) {
-                      setFile(e.target.files[0]);
-                      setFileName((e.target.files[0] || {}).name);
-                      setFileSize(` ${Math.round(e.target.files[0].size / 10000) / 100} MB `); // 1mb = 1000000
+                    const fileObj = e.target.files[0];
+                    if (fileObj) {
+                      setFile(fileObj);
+                      setFileName(fileObj.name);
+                      setFileSize(` ${(Math.round(fileObj.size / 10000) / 100) || '0.01'} MB `); // 1mb = 1000000
                     } else {
                       unattachFile({ value: '', error: false });
                     }
