bd62f4c6dbdd5b01ce1ea9cc977ee1bf6cca97f7
stefan9541 <stepi08@rambler.ru>
2020-05-05 11:51:11 +0300

    fix text intent

--- pages/tech-apps-react-native.js
+++ pages/tech-apps-react-native.js
@@ -6,6 +6,7 @@ import ContentNavigation from '../components/content/navigation';
 import Works from '../components/portfolio/works';
 import CallToActionButton from '../components/content/CallToActionButton';
 import FeaturesList from '../components/content/FeaturesList';
+import WhyKeenethicsList from '../components/content/WhyKeenethicsList';
 
 import { techApps, works } from '../main.config';
 
@@ -103,6 +104,54 @@ const servicesList = [
   },
 ];
 
+const whyKeenEthicsList = [
+  {
+    title: 'Empowering growth.',
+    desc: `
+    Developing native apps for iOS and Android is a time-consuming and expensive endeavor.
+    By choosing React Native app development, you save some budget for
+    other business initiatives. By helping you use resources more
+    efficiently, our React Native team helps your business grow.`,
+  },
+  {
+    title: 'Building value.',
+    desc: `
+    To establish a valuable relationship with users and clients,
+    you should communicate with them effectively and often.
+    A fast, responsive, and lightweight mobile app with push-notifications
+    is the best way to keep customers engaged.
+    With React Native, we will help you achieve this goal.`,
+  },
+  {
+    title: 'Launching in a matter of weeks.',
+    desc: `
+    The greatest advantage of React Native is resource-efficiency.
+    With this technology, we will develop two apps over the time needed
+    to develop one. Moreover, our partner engagement process is
+    optimized greatly to make sure that
+    little to no time is spent on repetitive conversations and courtesy calls.`,
+  },
+  {
+    title: 'Bringing tech expertise.',
+    desc: `
+    Despite all the advantages of it, there are not so many expert
+    React Native app development specialists. But we will provide
+    you with professionals needed to complete your project.
+    Our React Native development company offers not only
+    high-quality software solutions but also sound expertise
+    in your industry or business field.`,
+  },
+  {
+    title: 'Protecting your data.',
+    desc: `
+    Your users will never give your app a second chance if they have even
+    the slightest doubt about the safety of their information.
+    Our expert React Native application development experts
+    will make sure
+    that your app is protected against technical errors and hacking attempts.`,
+  },
+];
+
 const TechAppsRN = () => (
   <Layout>
     <ContentNavigation
@@ -128,11 +177,20 @@ const TechAppsRN = () => (
       <section>
         <h2>What Is React Native Application Development?</h2>
         <p>
-          There are two target audiences that the mobile app owner should aim at, – Android users and iOS users, – and neither one can be ignored. Developing a separate app for each platform is time-consuming, inconvenient,
-          and expensive. React Native solves this problem. By offering the opportunity for cross-platform development, React Native greatly reduces the amount of resources needed for the development of Android and iOS apps without compromising the app quality.
+          There are two target audiences that the mobile app
+          owner should aim at, – Android users and iOS
+          users, – and neither one can be ignored.
+          Developing a separate app for each platform is time-consuming, inconvenient,
+          and expensive. React Native solves this problem.
+          By offering the opportunity for cross-platform development,
+          React Native greatly reduces the amount of resources
+          needed for the development of Android and iOS apps without compromising the app quality.
         </p>
         <p>
-          React Native is a JavaScript-based cross-platform mobile app development framework. 95% of React Native codebase is shared by iOS and Android apps. Moreover, some business logic of a web app written in React can be reused with the help of React Native application development.
+          React Native is a JavaScript-based cross-platform mobile app development
+          framework. 95% of React Native codebase is shared by iOS and Android apps. Moreover,
+          some business logic of a web app written in React can be reused
+          with the help of React Native application development.
           It is faster, more effective, and more convenient.
         </p>
       </section>
@@ -164,52 +222,7 @@ const TechAppsRN = () => (
       <section>
         <h2>Why KeenEthics?</h2>
 
-        <h3 className="red-italic-title">
-          <span style={{ fontSize: '28px', marginRight: '.5em' }}>1</span>
-          Empowering growth.
-        </h3>
-        <p>
-          <p>
-            Developing native apps for iOS and Android is a time-consuming and expensive endeavor.
-            By choosing React Native app development, you save some budget for
-            other business initiatives. By helping you use resources more efficiently, our React Native team helps your business grow.
-          </p>
-        </p>
-
-        <h3 className="red-italic-title">
-          <span style={{ fontSize: '28px', marginRight: '.5em' }}>2</span>
-          Building value.
-        </h3>
-        <p>
-          To establish a valuable relationship with users and clients, you should communicate with them effectively and often. A fast, responsive, and lightweight mobile app with push-notifications is the best way to keep customers engaged.
-          With React Native, we will help you achieve this goal.
-        </p>
-
-        <h3 className="red-italic-title">
-          <span style={{ fontSize: '28px', marginRight: '.5em' }}>3</span>
-          Launching in a matter of weeks.
-        </h3>
-        <p>
-          The greatest advantage of React Native is resource-efficiency. With this technology, we will develop two apps over the time needed to develop one. Moreover, our partner engagement process is optimized greatly to make sure that
-          little to no time is spent on repetitive conversations and courtesy calls.
-        </p>
-        <h3 className="red-italic-title">
-          <span style={{ fontSize: '28px', marginRight: '.5em' }}>4</span>
-          Bringing tech expertise.
-        </h3>
-        <p>
-          Despite all the advantages of it, there are not so many expert React Native app development specialists. But we will provide you with professionals needed to complete your project. Our React Native development company offers not only
-          high-quality software solutions but also sound expertise in your industry or business field.
-        </p>
-
-        <h3 className="red-italic-title">
-          <span style={{ fontSize: '28px', marginRight: '.5em' }}>5</span>
-          Protecting your data.
-        </h3>
-        <p>
-          Your users will never give your app a second chance if they have even the slightest doubt about the safety of their information. Our expert React Native application development experts will make sure
-          that your app is protected against technical errors and hacking attempts.
-        </p>
+        <WhyKeenethicsList list={whyKeenEthicsList} />
       </section>
 
       <section>
@@ -217,9 +230,13 @@ const TechAppsRN = () => (
         <div>
           <p className="text-bold text-center">ParkQX</p>
           <p>
-            ParkQX is a mobile application aimed to facilitate the parking routine and to spare the unnecessary hustle.
-            On the one hand, it helps drivers find, book, and pay for a parking spot in a few simple taps. On the other hand, it helps parking spot owners profit from their unused parking space. This
-            mobile solution has the potential to improve the transport and parking situation in any city.
+            ParkQX is a mobile application aimed to facilitate the parking
+            routine and to spare the unnecessary hustle.
+            On the one hand, it helps drivers find, book,
+            and pay for a parking spot in a few simple taps. On the other hand,
+            it helps parking spot owners profit from their unused parking space. This
+            mobile solution has the potential to improve
+            the transport and parking situation in any city.
           </p>
           <div>
             <div style={{ marginBottom: '5px' }}>
