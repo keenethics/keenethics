d839787d4b40c7f06c1702e7f66eb7dc2e532901
stefan9541 <stepi08@rambler.ru>
2020-05-20 18:48:48 +0300

    add target=blank to link in footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -212,7 +212,11 @@ const HomeFooter = ({
             <li className="home-footer-list-item">
               <div className="flag-country-wrapper">
                 <span className="flag-country-wrapper-flag">
-                  <a href="https://www.google.com/maps/place/KeenEthics/@49.8269687,23.9880895,17z/data=!3m1!4b1!4m5!3m4!1s0x473ae7fa87f09d6f:0x9ac85f44cbcb9257!8m2!3d49.8269687!4d23.9902782">
+                  <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://www.google.com/maps/place/KeenEthics/@49.8269687,23.9880895,17z/data=!3m1!4b1!4m5!3m4!1s0x473ae7fa87f09d6f:0x9ac85f44cbcb9257!8m2!3d49.8269687!4d23.9902782"
+                  >
                     <img
                       src="/static/images/flag_ukraine.png"
                       alt="Flag"
@@ -221,12 +225,22 @@ const HomeFooter = ({
                   </a>
                 </span>
                 <div className="country-text">
-                  <a className="country-link" href="https://www.google.com/maps/place/KeenEthics/@49.8269687,23.9880895,17z/data=!3m1!4b1!4m5!3m4!1s0x473ae7fa87f09d6f:0x9ac85f44cbcb9257!8m2!3d49.8269687!4d23.9902782">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="country-link"
+                    href="https://www.google.com/maps/place/KeenEthics/@49.8269687,23.9880895,17z/data=!3m1!4b1!4m5!3m4!1s0x473ae7fa87f09d6f:0x9ac85f44cbcb9257!8m2!3d49.8269687!4d23.9902782"
+                  >
                     <span className="country" itemProp="addressRegion">
                       Ukraine
                     </span>
                   </a>
-                  <a className="address" href="https://www.google.com/maps/place/KeenEthics/@49.8269687,23.9880895,17z/data=!3m1!4b1!4m5!3m4!1s0x473ae7fa87f09d6f:0x9ac85f44cbcb9257!8m2!3d49.8269687!4d23.9902782">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="address"
+                    href="https://www.google.com/maps/place/KeenEthics/@49.8269687,23.9880895,17z/data=!3m1!4b1!4m5!3m4!1s0x473ae7fa87f09d6f:0x9ac85f44cbcb9257!8m2!3d49.8269687!4d23.9902782"
+                  >
                     Lviv, Kulparkivska St. 59
                   </a>
                 </div>
@@ -237,7 +251,11 @@ const HomeFooter = ({
             <li className="home-footer-list-item">
               <div className="flag-country-wrapper">
                 <span className="flag-country-wrapper-flag">
-                  <a href="https://www.google.com/maps/place/KeenEthics/@51.7713527,5.5118959,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6fdf815ac947b:0xf0d1841b4903762b!8m2!3d51.7713569!4d5.5141752">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.google.com/maps/place/KeenEthics/@51.7713527,5.5118959,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6fdf815ac947b:0xf0d1841b4903762b!8m2!3d51.7713569!4d5.5141752"
+                  >
                     <img
                       src="/static/images/flag_netherlands.png"
                       alt="Flag Netherlands"
@@ -246,12 +264,22 @@ const HomeFooter = ({
                   </a>
                 </span>
                 <div className="country-text">
-                  <a className="country-link" href="https://www.google.com/maps/place/KeenEthics/@51.7713527,5.5118959,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6fdf815ac947b:0xf0d1841b4903762b!8m2!3d51.7713569!4d5.5141752">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="country-link"
+                    href="https://www.google.com/maps/place/KeenEthics/@51.7713527,5.5118959,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6fdf815ac947b:0xf0d1841b4903762b!8m2!3d51.7713569!4d5.5141752"
+                  >
                     <span className="country" itemProp="addressRegion">
                       Netherlands
                     </span>
                   </a>
-                  <a className="address" href="https://www.google.com/maps/place/KeenEthics/@51.7713527,5.5118959,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6fdf815ac947b:0xf0d1841b4903762b!8m2!3d51.7713569!4d5.5141752">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="address"
+                    href="https://www.google.com/maps/place/KeenEthics/@51.7713527,5.5118959,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6fdf815ac947b:0xf0d1841b4903762b!8m2!3d51.7713569!4d5.5141752"
+                  >
                     Oss, Oude litherweg 2, 5346 RT
                   </a>
                 </div>
@@ -262,7 +290,11 @@ const HomeFooter = ({
             <li className="home-footer-list-item">
               <div className="flag-country-wrapper">
                 <span className="flag-country-wrapper-flag">
-                  <a href="https://www.google.com/maps/place/Workville/@40.7536262,-73.9887379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259ab050f01bb:0xc4f59321f603c1a3!8m2!3d40.7536222!4d-73.9865492">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.google.com/maps/place/Workville/@40.7536262,-73.9887379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259ab050f01bb:0xc4f59321f603c1a3!8m2!3d40.7536222!4d-73.9865492"
+                  >
                     <img
                       src="/static/images/united-states.svg"
                       alt="Flag USA"
@@ -271,12 +303,22 @@ const HomeFooter = ({
                   </a>
                 </span>
                 <div className="country-text">
-                  <a className="country-link" href="https://www.google.com/maps/place/Workville/@40.7536262,-73.9887379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259ab050f01bb:0xc4f59321f603c1a3!8m2!3d40.7536222!4d-73.9865492">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="country-link"
+                    href="https://www.google.com/maps/place/Workville/@40.7536262,-73.9887379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259ab050f01bb:0xc4f59321f603c1a3!8m2!3d40.7536222!4d-73.9865492"
+                  >
                     <span className="country" itemProp="addressRegion">
                       USA
                     </span>
                   </a>
-                  <a className="address" href="https://www.google.com/maps/place/Workville/@40.7536262,-73.9887379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259ab050f01bb:0xc4f59321f603c1a3!8m2!3d40.7536222!4d-73.9865492">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="address"
+                    href="https://www.google.com/maps/place/Workville/@40.7536262,-73.9887379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259ab050f01bb:0xc4f59321f603c1a3!8m2!3d40.7536222!4d-73.9865492"
+                  >
                     New York, 1412 Broadway, 21st floor, 2200
                   </a>
                 </div>
