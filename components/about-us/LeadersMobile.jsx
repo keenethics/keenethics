449e62974f3e2d7285e51c1e25ede69ab4bcc0d9
stefan9541 <stepi08@rambler.ru>
2020-06-01 12:19:25 +0300

    fix incorrect active dot item, Cleaned the code

--- components/about-us/LeadersMobile.jsx
+++ components/about-us/LeadersMobile.jsx
@@ -1,25 +1,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
-import { times } from 'lodash';
-
-// dotsClass: 'values-toggler mobile',
-// customPaging(i) {
-//   return (
-//     <a>
-//       <li className="active" />
-//     </a>
-//   );
-// },
-// slidesToScroll: 2,
-// appendDots: (dots) => (
-//   <ul className="values-toggler mobile">
-//     {dots}
-//   </ul>
-// ),
-// };
 
 const LeadersMobile = ({ data }) => {
   const [step, setStep] = useState(0);
@@ -32,66 +15,18 @@ const LeadersMobile = ({ data }) => {
   };
 
   const settings = {
-    // dots: true,
     infinite: true,
     speed: 500,
     arrows: false,
     slidesToShow: 2,
     beforeChange: (_, toStep) => {
-      stepper(toStep);
+      stepper(toStep / 2);
     },
   };
-
-  const handleScroll = ({ target: { scrollLeft, clientWidth, scrollWidth } }) => {
-    const scrollPercentage = Math.round(
-      ((scrollLeft + clientWidth) / scrollWidth) * 100,
-    );
-    let toStep = 0;
-    times(5, (index) => {
-      let computedPercentage = scrollPercentage;
-      if (index === 0) {
-        computedPercentage = Math.round(
-          (scrollLeft / scrollWidth) * 100,
-        );
-      }
-      if (computedPercentage > (index + 1) * 20) {
-        toStep += 1;
-      }
-    });
-    setStep(toStep);
-  };
-  // const gallery = useRef(null);
-  // useEffect(() => {
-  //   if (gallery && gallery.current) {
-  //     gallery.current.addEventListener('scroll', handleScroll);
-  //   }
-
-  //   return () => {
-  //     if (gallery && gallery.current) {
-  //       gallery.current.removeEventListener('scroll', handleScroll);
-  //     }
-  //   };
-  // }, [gallery]);
-
-  // const handleNavClick = (toStep) => {
-  //   // let stepper = 0;
-  //   // if (toStep <= 0) {
-  //   //   stepper = toStep;
-  //   // } else {
-  //   //   stepper = toStep + 1;
-  //   // }
-  //   // if (toStep >= ourLeaders.length - 1) stepper = toStep;
-  //   gallery.current.children[0].children[toStep].scrollIntoView({
-  //     behavior: 'smooth',
-  //     inline: 'start',
-  //     block: 'nearest',
-  //   });
-  // };
 
   return (
     <>
       <div className="gallery mobile">
-        {/* <div className="gallery-inner"> */}
         <Slider
           swipeToSlide
           className="hello"
@@ -120,7 +55,6 @@ const LeadersMobile = ({ data }) => {
             ))
           }
         </Slider>
-        {/* </div> */}
       </div>
 
       <ul className="values-toggler mobile">
@@ -128,7 +62,7 @@ const LeadersMobile = ({ data }) => {
           ourLeaders.slice(0, 3).map((_, i) => (
             <li
               key={i}
-              className={i * 2 <= step ? 'active' : ''}
+              className={i <= step ? 'active' : ''}
               onClick={() => stepper(i)}
             />
           ))
