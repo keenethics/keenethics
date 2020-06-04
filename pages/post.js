76750e1409702986296ef68b6fefe8512f87e717
stefan9541 <stepi08@rambler.ru>
2020-06-04 15:14:58 +0300

    add noFollow rel to blog post link

--- pages/post.js
+++ pages/post.js
@@ -153,7 +153,24 @@ const bodyOptions = {
         return filteredChildren[0];
       }
 
-      return <p>{children.filter((item) => !!item)}</p>;
+      return (
+        <p>
+          {children.reduce((acc, item) => {
+            if (typeof item === 'object' && item.type === 'a') {
+              const { props: { children: child, href } } = item;
+              const isNeddedNofollow = href.includes('https://keenethics.com');
+              if (isNeddedNofollow) {
+                acc.push(<a href={href}>{child}</a>);
+              } else {
+                acc.push(<a rel="nofollow" href={href}>{child}</a>);
+              }
+            } else if (item) {
+              acc.push(item);
+            }
+            return acc;
+          }, [])}
+        </p>
+      );
     },
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
       const { url } = node.data.target.fields.file;
@@ -301,6 +318,7 @@ export default class Post extends React.Component {
       metaTitle,
       metaDescription,
     } = this.props;
+    console.log(bodyRich);
     const heroSrc = _.get(heroImage, 'fields.file.url', '');
     const heroTitle = _.get(heroImage, 'fields.title', '');
 
