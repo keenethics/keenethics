7bd229579a48496628074da5afb63dd69c97325b
stefan9541 <stepi08@rambler.ru>
2020-07-09 15:43:22 +0300

    fix lint error

--- helpers/date-parser.js
+++ helpers/date-parser.js
@@ -1,9 +1,22 @@
 /* eslint-disable camelcase */
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
 function format_D_MMM_YYYY(year, month, day) {
-  day < 10 ? day = day.slice(1) : day;
-  return `${day} ${month.slice(0, 3)}, ${year}`;
+  const slicedDay = (day < 10) ? day.slice(1) : day;
+  return `${slicedDay} ${month.slice(0, 3)}, ${year}`;
 }
 
 function format_MMM_DD_YYYY(year, month, day) {
@@ -16,7 +29,7 @@ function format_MMMM_DD_YYYY(year, month, day) {
 
 function dateParse(date, format) {
   const [year, month, day] = date.split('-');
-  const parseMonth = (month < 10) ? month[1] : month;
+  const parseMonth = month < 10 ? month[1] : month;
   // if the moth = 01,02,03 return 1,2,3 ;
   const actualMonth = months[parseMonth - 1];
 
@@ -28,7 +41,9 @@ function dateParse(date, format) {
     case 'MMMM DD YYYY':
       return format_MMMM_DD_YYYY(year, actualMonth, day);
     default:
-      throw new Error('available formats is (D MMM YYY, MMM DD YYYY, MMMM DD YYYY)');
+      throw new Error(
+        'available formats is (D MMM YYY, MMM DD YYYY, MMMM DD YYYY)',
+      );
   }
 }
 
