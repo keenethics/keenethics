78de9243fa79829ff52f6986f2ab67079273fe79
Melnychenko Vitalii <vitaliy.melnychenko@keenethics.com>
2020-07-20 14:24:18 +0300

    implement github cd

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 const withSass = require('@zeit/next-sass');
-const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
-
-require('dotenv-safe').config();
 
 module.exports = withSass({
   generateBuildId: async () => `build-${Date.now()}`,
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin([
-      'REACT_APP_SPACE_ID',
-      'REACT_APP_ACCESS_TOKEN',
-      'REACT_APP_PREVIEW_TOKEN',
-    ]));
-    config.plugins.push(new Dotenv());
+    config.plugins.push(new Dotenv({ path: process.env.ENV_PATH }));
 
     return config;
   },
