0d30eee633d2574af5b555342263746d23700c14
Vitalii Melnychenko <vitaliy.melnychenko@keenethics.com>
2020-09-22 11:02:23 +0300

    disable form button if not filled

--- pages/referral-program.js
+++ pages/referral-program.js
@@ -636,6 +636,7 @@ const ReferralProgram = () => {
                 role="presentation"
                 className="button orange-btn"
                 onClick={() => validateForm()}
+                disabled={!selectedDate || !country || !selectedTime ? 'disabled' : ''}
               >
                 Next
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
